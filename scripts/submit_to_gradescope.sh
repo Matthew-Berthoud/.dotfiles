ccfa145df13da0eb8eb682918c829da9cd5c80a0
Matthew Berthoud <matthewberthoud@gmail.com>
2025-02-06 17:17:48 -0500

    clean up zip on remote machine

--- scripts/submit_to_gradescope.sh
+++ scripts/submit_to_gradescope.sh
@@ -5,8 +5,10 @@ set -e
 project="sget"
 server="bg3"
 project_folder="~/Distributed-System-Security/Projects/$project"
-remote_command="cd $project_folder && make clean && zip -r $project.zip *"
+make_zip="cd $project_folder && make clean && zip -r $project.zip *"
+rm_zip="cd $project_folder && rm $project.zip"
 
-ssh "$server" "$remote_command"
+ssh "$server" "$make_zip"
 scp "$server:$project_folder.zip" ~/Downloads/
+ssh "$server" "$rm_zip"
 
