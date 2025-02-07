fddc3ed051d4763206b85a5f8a41a1d522ec87ef
Matthew Berthoud <matthewberthoud@gmail.com>
2025-02-07 16:51:04 -0500

    fixed submission script

--- scripts/submit_to_gradescope.sh
+++ scripts/submit_to_gradescope.sh
@@ -7,8 +7,9 @@ server="bg3"
 project_folder="~/Distributed-System-Security/Projects/$project"
 make_zip="cd $project_folder && make clean && zip -r $project.zip *"
 rm_zip="cd $project_folder && rm $project.zip"
+local_file="$HOME/Downloads/$project.zip"
 
 ssh "$server" "$make_zip"
-scp "$server:$project_folder.zip" ~/Downloads/
+scp "$server:$project_folder/$project.zip" "$local_file"
 ssh "$server" "$rm_zip"
 
