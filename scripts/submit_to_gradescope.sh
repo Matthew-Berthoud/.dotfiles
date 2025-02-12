e9e8f3610fa34c7936656e835a3d13ed4b0a8d9f
Matthew Berthoud <matthewberthoud@gmail.com>
2025-02-12 17:22:51 -0500

    submit to gradescope fixed

--- scripts/submit_to_gradescope.sh
+++ scripts/submit_to_gradescope.sh
@@ -5,11 +5,8 @@ set -e
 project="sget"
 server="bg3"
 project_folder="~/Distributed-System-Security/Projects/$project"
-make_zip="cd $project_folder && make clean && zip -r $project.zip *"
-rm_zip="cd $project_folder && rm $project.zip"
-local_file="$HOME/Downloads/$project.zip"
 
-ssh "$server" "$make_zip"
-scp "$server:$project_folder/$project.zip" "$local_file"
-ssh "$server" "$rm_zip"
+ssh "$server" "cd $project_folder && make clean && zip -r $project.zip *"
+scp "$server:$project_folder/$project.zip" "$HOME/Downloads/$project.zip"
+ssh "$server" "cd $project_folder && rm $project.zip"
 
