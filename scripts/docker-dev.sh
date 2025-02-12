2b0ab04addf3e1b38792a1f09abf542f89678b79
Matthew Berthoud <matthewberthoud@gmail.com>
2025-02-12 18:39:31 -0500

    react docker dev env

--- scripts/docker-dev.sh
+++ scripts/docker-dev.sh
@@ -5,8 +5,17 @@ set -e
 DOTFILES="$HOME/.dotfiles"
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <language>"
+  echo "Usage: $0 <language> <host port (optional)> <container port (optional)>"
   exit 1
+fi
+
+CONTAINER_PORT="3000"
+HOST_PORT="3000"
+if [ -n "$2" ]; then
+  HOST_PORT=$2
+fi
+if [ -n "$3" ]; then
+  CONTAINER_PORT=$3
 fi
 
 LANGUAGE=$1
@@ -25,5 +34,6 @@ docker run -it --rm \
     -v ~/.ssh/id_ed25519:/home/mwberthoud/.ssh/id_ed25519 \
     -v ~/.ssh/id_ed25519.pub:/home/mwberthoud/.ssh/id_ed25519.pub \
     -v "$LANGUAGE-workspace:/workspace" \
+    -p "$HOST_PORT":"$CONTAINER_PORT"\
     "$IMAGE_NAME"
 
