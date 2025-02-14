d5951f4fee3fad9d71a8aae72d3d9b0c113a6ed7
Matthew Berthoud <matthewberthoud@gmail.com>
2025-02-13 21:20:34 -0500

    test

--- docker-dev/container_setup.sh
+++ docker-dev/container_setup.sh
@@ -6,8 +6,8 @@
 set -e
 
 cd $HOME/.dotfiles
-bash scripts/install.sh
 git remote set-url origin git@github.com:Matthew-Berthoud/.dotfiles.git
+source scripts/install.sh
 
 git config --global user.email "mwberthoud@wm.edu"
 git config --global user.name "Matthew Berthoud"
