c1fe669521937c67370a86171a6a3a8a430a8892
Matthew Berthoud <matthewberthoud@gmail.com>
2025-02-13 21:40:29 -0500

    just make ssh folder a volume, whatever

--- docker-dev/launch.sh
+++ docker-dev/launch.sh
@@ -33,9 +33,7 @@ docker build -t "$IMAGE_NAME" -f "$DOCKERFILE_PATH" .
 
 # SSH keys are stored as a docker volume so I can access private repos from the dev container
 docker run -it --rm \
-    -v ~/.ssh/id_ed25519:/home/mwberthoud/.ssh/id_ed25519 \
-    -v ~/.ssh/id_ed25519.pub:/home/mwberthoud/.ssh/id_ed25519.pub \
-    -v "$IMAGE_NAME-known_hosts:/home/mwberthoud/.ssh/known_hosts" \
+    -v ~/.ssh:/home/mwberthoud/.ssh \
     -v "$IMAGE_NAME-workspace:/workspace" \
     -p "$HOST_PORT":"$CONTAINER_PORT"\
     "$IMAGE_NAME"
