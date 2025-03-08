8762c917d17ff3852f776e134c11478943764e4e
Matthew Berthoud <matthewberthoud@gmail.com>
2025-03-07 23:09:10 -0700

    need to use --rm flag for now I think

--- docker-dev/launch.sh
+++ docker-dev/launch.sh
@@ -32,7 +32,7 @@ IMAGE_NAME="$LANGUAGE-ddev"
 docker build -t "$IMAGE_NAME" -f "$DOCKERFILE_PATH" .
 
 # SSH keys are stored as a docker volume so I can access private repos from the dev container
-docker run -it \
+docker run -it --rm \
     -v ~/.ssh:/home/mwberthoud/.ssh \
     -v ~/repos:/repos \
     -v "$LANGUAGE-go-folder:/go" \
