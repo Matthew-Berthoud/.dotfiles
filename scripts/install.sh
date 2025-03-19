b88c9098b762c7b0d8f47f58806ef8bbbe6ab98d
Matthew Berthoud <matthewberthoud@gmail.com>
2025-03-19 19:26:36 -0400

    editor nvim by default

--- scripts/install.sh
+++ scripts/install.sh
@@ -31,5 +31,5 @@ if [[ "$(uname)" == "Darwin" ]]; then
     brew bundle
 fi
 
-
-
+# Set default editor to nvim if it exists. Otherwise set to vim
+command -v nvim >/dev/null 2>&1 && export EDITOR=nvim || export EDITOR=vim
